8a12bea1238d6f87f7fc63af8c6cf8d72cc230f4
Leonardo Lima <leolimaf@outlook.com>
2024-07-27 18:40:34 -0300

    .

--- WordOnline/Controllers/HomeController.cs
+++ WordOnline/Controllers/HomeController.cs
@@ -1,13 +1,34 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WordOnline.Data;
 using WordOnline.Models;
 
 namespace WordOnline.Controllers;
 
-public class HomeController : Controller
+public class HomeController(AppDbContext context) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
+    {
+        var documentos = await context.Documentos.ToListAsync();
+        
+        return View(documentos);
+    }
+
+    public IActionResult CriarDocumento()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CriarDocumento(Documento documento)
+    {
+        if (!ModelState.IsValid) 
+            return View(documento);
+        
+        await context.AddAsync(documento);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+
     }
 }
