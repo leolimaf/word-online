5668c89093f19c39dcae8322890bab6dfd5708fd
Leonardo Lima <leolimaf@outlook.com>
2024-08-05 16:11:24 -0300

    .

--- WordOnline/Controllers/HomeController.cs
+++ WordOnline/Controllers/HomeController.cs
@@ -31,4 +31,36 @@ public class HomeController(AppDbContext context) : Controller
         return RedirectToAction("Index");
 
     }
+    public async Task<IActionResult> EditarDocumento(Guid id)
+    {
+        var documento = await context.Documentos.FindAsync(id);
+        return View(documento);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditarDocumento(Documento documentoEditado)
+    {
+        if (!ModelState.IsValid) 
+            return View();
+        
+        var documento = await context.Documentos.FindAsync(documentoEditado.Id);
+
+        documento.Titulo = documentoEditado.Titulo;
+        documento.Conteudo = documentoEditado.Conteudo;
+        documento.DataAlteracao = DateTime.Now;
+
+        context.Update(documento);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> RemoverDocumento(Guid id)
+    {
+        var documento = await context.Documentos.FindAsync(id);
+        context.Documentos.Remove(documento);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
 }
